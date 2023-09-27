0830e6e120f688c72ee58f9e69bb94ea695980eb
Masuma01~ <javariamasooma.bscssef20@iba-suk.edu.pk>
2023-09-27 15:19:14 +0500

    completed some tasks

--- task-2/script.js
+++ task-2/script.js
@@ -1,4 +1,22 @@
 function getAttributes(){
     const link = document.getElementById('w3r');
-}
+    const type = link.getAttribute('type');
+    const hreflang = link.getAttribute('hreflang');
+    const rel = link.getAttribute('rel');
+    const target = link.getAttribute('target');
+    const href = link.getAttribute('href');
+    // console.log(type);
+    // console.log(hreflang);
+    // console.log(rel);
+    // console.log(target);
+    // console.log(href);
+    return {
+        link: link,
+        type: type,
+        hreflang: hreflang,
+        rel: rel,
+        target: target,
+        href: href
+    }
 
+}
---
+++ task-3/script.js
@@ -0,0 +1,7 @@
+function js_style(){
+    const para=document.querySelector('p');
+    para.style.color='blue';
+    para.style.fontFamily='fantasy';
+    para.style.fontSize='30px';
+    para.style.background='lightblue';
+}
